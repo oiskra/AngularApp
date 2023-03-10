00a66c32a4b75385537f25331579e85e9f51efb7
oiskra <ojaskra@gmail.com>
2023-03-10 09:47:20 +0100

    lab2

--- Lab_2/src/app/app.module.ts
+++ Lab_2/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SmallCardComponent } from './small-card/small-card.component';
+import { MediumCardComponent } from './medium-card/medium-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmallCardComponent,
+    MediumCardComponent
   ],
   imports: [
     BrowserModule
