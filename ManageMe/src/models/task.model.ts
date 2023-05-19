3a7c39e5c4bf1aef16c824df05132aedd519f64d
oiskra <ojaskra@gmail.com>
2023-05-19 21:24:13 +0200

    model setup

--- ManageMe/src/models/task.model.ts
+++ ManageMe/src/models/task.model.ts
@@ -1,8 +1,16 @@
 import { Functionality } from "./functionality.model";
+import { State } from "./state.model";
+import { User } from "./user.model";
 
 export type Task = {
     name: string;
     description: string;
     priority: number;
     functionality: Functionality;
+    estimatedTimeOfCompletion: string;
+    state: State;
+    addedAt: Date;
+    startedAt: Date; //stan zmieniony na doing
+    finishedAt: Date; //stan zmieniony na done
+    assignedEmployee: User;
 }
