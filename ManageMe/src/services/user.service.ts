5024a510674c6ee7ea44bf2650d35e0bf15dd6b6
oiskra <ojaskra@gmail.com>
2023-05-21 14:16:11 +0200

    created user service

---
+++ ManageMe/src/services/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { User } from 'src/models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private static _users: User[];
+
+  constructor() { }
+}
