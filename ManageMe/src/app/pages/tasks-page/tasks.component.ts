7ece544644243b87717f2859d56cbf33b36e9b83
oiskra <ojaskra@gmail.com>
2023-06-06 22:58:15 +0200

    update functionality on task state update refactor

--- ManageMe/src/app/pages/tasks-page/tasks.component.ts
+++ ManageMe/src/app/pages/tasks-page/tasks.component.ts
@@ -1,17 +1,24 @@
-import { CdkDragDrop, CdkDragStart, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  CdkDragStart,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Subscription, map } from 'rxjs';
+import { Functionality } from 'src/models/functionality.model';
 import { State } from 'src/models/state.model';
 import { Task } from 'src/models/task.model';
 import { AuthService } from 'src/services/auth.service';
+import { FunctionalityService } from 'src/services/functionality.service';
 import { TaskService } from 'src/services/task.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit, OnDestroy {
   protected tasks: Task[] = [];
@@ -25,21 +32,28 @@ export class TasksComponent implements OnInit, OnDestroy {
 
   constructor(
     private auth: AuthService,
-    private taskService: TaskService, 
+    private taskService: TaskService,
     private router: Router,
-    private snackBar: MatSnackBar) { }
+    private snackBar: MatSnackBar,
+    private functionalityService: FunctionalityService
+  ) {}
 
   ngOnInit(): void {
-    this.taskSub$ = this.taskService.getAllWorkingTasks().subscribe(data => {
-      this.tasks = [...data]
-      
-      if(data.length === [...this.tasksToDo, ...this.tasksDone, ...this.tasksDoing].length) {return;}
-      
+    this.taskSub$ = this.taskService.getAllWorkingTasks().subscribe((data) => {
+      this.tasks = [...data];
+
+      if (
+        data.length ===
+        [...this.tasksToDo, ...this.tasksDone, ...this.tasksDoing].length
+      ) {
+        return;
+      }
+
       this.tasksToDo = [];
       this.tasksDoing = [];
       this.tasksDone = [];
 
-      data.forEach(task => {
+      data.forEach((task) => {
         switch (task.task_state) {
           case 'TODO':
             this.tasksToDo.push(task);
@@ -51,12 +65,14 @@ export class TasksComponent implements OnInit, OnDestroy {
             this.tasksDone.push(task);
             break;
         }
-      })
-    });  
+      });
+    });
 
-    this.loggedUserIdSub$ = this.auth.loggedUser$.pipe(
-      map(user => user!.user_id)
-    ) .subscribe(id => {this.loggedUserId = id});
+    this.loggedUserIdSub$ = this.auth.loggedUser$
+      .pipe(map((user) => user!.user_id))
+      .subscribe((id) => {
+        this.loggedUserId = id;
+      });
   }
 
   ngOnDestroy(): void {
@@ -69,48 +85,91 @@ export class TasksComponent implements OnInit, OnDestroy {
   }
 
   changeState(event: CdkDragDrop<Task[]>) {
-    if(event.previousContainer.data[event.previousIndex].task_assignedEmployeeId !== this.loggedUserId) {
-      this.snackBar.open("You can't update tasks that are not assigned to you", undefined, {
-        duration: 2000
-      })
+    if (
+      event.previousContainer.data[event.previousIndex]
+        .task_assignedEmployeeId !== this.loggedUserId
+    ) {
+      this.snackBar.open(
+        "You can't update tasks that are not assigned to you",
+        undefined,
+        {
+          duration: 2000,
+        }
+      );
       return;
     }
 
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
 
-      const updatedNonObservedTaskId: number = event.container.data[event.currentIndex].task_ID;
-      const updatedTask: Task = this.tasks.find(task => task.task_ID === updatedNonObservedTaskId)! 
-      const updatedState: State = event.container.id as State;
+      this.updateState(event);
+    }
+  }
 
-      const updatedTaskClone: Task = {...updatedTask};
-      updatedTaskClone.task_state = updatedState;
+  private updateState(event: CdkDragDrop<Task[]>) {
+    const updatedNonObservedTaskId: number =
+      event.container.data[event.currentIndex].task_ID;
+    const updatedTask: Task = this.tasks.find(
+      (task) => task.task_ID === updatedNonObservedTaskId
+    )!;
+    const updatedState: State = event.container.id as State;
 
-      switch(updatedState) {
-        case 'TODO':
-          updatedTaskClone.task_startedAt = undefined;
+    const updatedTaskClone: Task = { ...updatedTask };
+    updatedTaskClone.task_state = updatedState;
+
+    switch (updatedState) {
+      case 'TODO':
+        updatedTaskClone.task_startedAt = undefined;
+        updatedTaskClone.task_finishedAt = undefined;
+        break;
+      case 'DOING':
+        if (updatedTaskClone.task_startedAt) {
           updatedTaskClone.task_finishedAt = undefined;
           break;
-        case 'DOING':
-          if(updatedTaskClone.task_startedAt) {
-            updatedTaskClone.task_finishedAt = undefined;
-            break;
-          }
-          updatedTaskClone.task_startedAt = new Date(Date.now());
-          break;
-        case 'DONE':
-          updatedTaskClone.task_finishedAt = new Date(Date.now());
-          break;
-      }
-
-      this.taskService.updateTask(updatedTask.task_ID, updatedTaskClone); 
+        }
+        updatedTaskClone.task_startedAt = new Date(Date.now());
+        break;
+      case 'DONE':
+        updatedTaskClone.task_finishedAt = new Date(Date.now());
+        break;
     }
+
+    this.taskService.updateTask(updatedTask.task_ID, updatedTaskClone);
+
+    const relatedFunctionality: Functionality =
+      this.functionalityService.getFunctionality(
+        updatedTask.task_functionalityId
+      )!;
+
+    this.taskService
+      .getRelatedFunctionalityTasks(relatedFunctionality.functionality_ID)
+      .subscribe((data) => {
+        const clone = { ...relatedFunctionality };
+
+        if (data.every((task) => task.task_state === 'TODO')) {
+          clone.functionality_state = 'TODO';
+        } else if (data.every((task) => task.task_state === 'DONE')) {
+          clone.functionality_state = 'DONE';
+        } else if (data.some((task) => task.task_state === 'DOING')) {
+          clone.functionality_state = 'DOING';
+        }
+
+        this.functionalityService.updateFunctionality(
+          relatedFunctionality.functionality_ID,
+          clone
+        );
+      })
+      .unsubscribe();
   }
 }
--- ManageMe/src/services/functionality.service.ts
+++ ManageMe/src/services/functionality.service.ts
@@ -4,72 +4,57 @@ import { Functionality } from 'src/models/functionality.model';
 import { TaskService } from './task.service';
 import { GlobalStateService } from './global-state.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FunctionalityService {
+  private _functionalities: BehaviorSubject<Functionality[]> =
+    new BehaviorSubject<Functionality[]>([
+      {
+        functionality_ID: 0,
+        functionality_name: 'functionality0',
+        functionality_description: 'desc0',
+        functionality_priority: 1,
+        functionality_projectId: 1,
+        functionality_ownerId: 1,
+        functionality_state: 'TODO',
+      },
+      {
+        functionality_ID: 1,
+        functionality_name: 'functionality1',
+        functionality_description: 'desc1',
+        functionality_priority: 1,
+        functionality_projectId: 1,
+        functionality_ownerId: 1,
+        functionality_state: 'TODO',
+      },
+      {
+        functionality_ID: 2,
+        functionality_name: 'functionality2',
+        functionality_description: 'desc2',
+        functionality_priority: 1,
+        functionality_projectId: 1,
+        functionality_ownerId: 1,
+        functionality_state: 'TODO',
+      },
+      {
+        functionality_ID: 3,
+        functionality_name: 'Example Functionality',
+        functionality_description: 'This is an example functionality.',
+        functionality_priority: 2,
+        functionality_projectId: 1,
+        functionality_ownerId: 1,
+        functionality_state: 'TODO',
+      },
+    ]);
 
-  private _functionalities: BehaviorSubject<Functionality[]> = new BehaviorSubject<Functionality[]>([
-    {
-      functionality_ID: 0,
-      functionality_name: 'functionality0',
-      functionality_description: 'desc0',
-      functionality_priority: 1,
-      functionality_projectId: 1,
-      functionality_ownerId: 1,
-      functionality_state: 'TODO'
-    },
-    {
-      functionality_ID: 1,
-      functionality_name: 'functionality1',
-      functionality_description: 'desc1',
-      functionality_priority: 1,
-      functionality_projectId: 1,
-      functionality_ownerId: 1,
-      functionality_state: 'TODO'
-    },
-    {
-      functionality_ID: 2,
-      functionality_name: 'functionality2',
-      functionality_description: 'desc2',
-      functionality_priority: 1,
-      functionality_projectId: 1,
-      functionality_ownerId: 1,
-      functionality_state: 'TODO'
-    },
-    {
-      functionality_ID: 3,
-      functionality_name: 'Example Functionality',
-      functionality_description: 'This is an example functionality.',
-      functionality_priority: 2,
-      functionality_projectId: 1,
-      functionality_ownerId: 1,
-      functionality_state: 'TODO'
-    }
-  ]);
-
-  private functionalities$: Observable<Functionality[]> = this._functionalities.asObservable();
+  private functionalities$: Observable<Functionality[]> =
+    this._functionalities.asObservable();
 
   constructor(
-    private taskService: TaskService, 
-    private globalState: GlobalStateService) {
-    this._functionalities.getValue().forEach(func => {
-      this.taskService.getRelatedFunctionalityTasks(func.functionality_ID).subscribe(data => {
-        const clone = {...func}
-        if(data.every(task => task.task_state === 'TODO')) {
-            clone.functionality_state = 'TODO';
-            this.updateFunctionality(func.functionality_ID, clone);
-        } else if(data.some(task => task.task_state === 'DOING')) {
-            clone.functionality_state = 'DOING';
-            this.updateFunctionality(func.functionality_ID, clone);
-        } else if(data.every(task => task.task_state === 'DONE')){
-            clone.functionality_state = 'DONE';
-            this.updateFunctionality(func.functionality_ID, clone);
-        }     
-      })
-    })   
-  }
+    private taskService: TaskService,
+    private globalState: GlobalStateService
+  ) {}
 
   getAllFunctionalities(): Observable<Functionality[]> {
     return this.functionalities$;
@@ -79,16 +64,20 @@ export class FunctionalityService {
     return this.functionalities$.pipe(
       switchMap((data) => {
         return this.globalState.workingProject$.pipe(
-          map(workingProjectId => {
-            return data.filter(func => func.functionality_projectId === workingProjectId)
+          map((workingProjectId) => {
+            return data.filter(
+              (func) => func.functionality_projectId === workingProjectId
+            );
           })
-        )
+        );
       })
     );
   }
 
   getFunctionality(id: number) {
-    return this._functionalities.getValue().find(func => func.functionality_ID === id)
+    return this._functionalities
+      .getValue()
+      .find((func) => func.functionality_ID === id);
   }
 
   createFunctionality(newFunctionality: Functionality) {
@@ -96,30 +85,35 @@ export class FunctionalityService {
   }
 
   updateFunctionality(id: number, functionality: Functionality) {
-    if(id !== functionality.functionality_ID) {
+    if (id !== functionality.functionality_ID) {
       return;
     }
 
-    const index: number = this._functionalities.getValue().findIndex(item => item.functionality_ID === id)
+    const index: number = this._functionalities
+      .getValue()
+      .findIndex((item) => item.functionality_ID === id);
     this._functionalities.getValue()[index] = functionality;
+    this._functionalities.next(this._functionalities.getValue());
   }
 
   deleteFunctionality(id: number) {
-    const index: number = this._functionalities.getValue().findIndex((item) => item.functionality_ID === id);
-    if(index < 0) {
+    const index: number = this._functionalities
+      .getValue()
+      .findIndex((item) => item.functionality_ID === id);
+    if (index < 0) {
       return;
     }
     this._functionalities.getValue().splice(index, 1);
     this._functionalities.next(this._functionalities.getValue());
 
-    this.taskService.getRelatedFunctionalityTasks(id)
-      .pipe(
-        map(data => data.map(task => task.task_ID))
-      )
-      .subscribe(data => {
+    this.taskService
+      .getRelatedFunctionalityTasks(id)
+      .pipe(map((data) => data.map((task) => task.task_ID)))
+      .subscribe((data) => {
         for (const taskId of data) {
           this.taskService.deleteTask(taskId);
         }
-      }).unsubscribe();
+      })
+      .unsubscribe();
   }
 }
